d3a93710011a49f9a5872d8b35e3a85dc7dd051e
unknown <joacopadins@gmail.com>
2022-08-21 20:16:13 -0300

    1 entrega

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Header from "./componentes/Header"
+
 
 function App() {
   return (
-    <p>Hola VERTICAL!!!</p>
+   <Header />
   );
 }
 
---
+++ src/componentes/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import App from "../App";
+
+const Header = () => {
+    return (
+        <div>
+            <p>HOLA VERTICAL</p>
+        </div>
+    );
+}
+
+export default Header;
