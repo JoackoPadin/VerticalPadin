bac0472f031432f403b6e5b764fe2b6616fd2e29
unknown <joacopadins@gmail.com>
2022-09-16 09:04:29 -0300

    rutas nueva rama arreglo dev

--- src/componentes/ItemList/ItemList.jsx
+++ src/componentes/ItemList/ItemList.jsx
@@ -5,7 +5,7 @@ const ItemList = ({items}) => {
   return (
     <div className='row'>
       {items.map(item => ( <div key={item.id} className="col-md-4 py-3">
-        <Item loteo={item.loteo} imagen={item.imagen} precio={item.precio} numeroDeLote={item.numeroDeLote}  />
+        <Item id={item.id} loteo={item.loteo} imagen={item.imagen} precio={item.precio} numeroDeLote={item.numeroDeLote}  />
         </div>)
         )}
     </div>
